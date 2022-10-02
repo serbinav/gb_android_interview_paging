e3304b7236046ed2828fcd303d8b328fd4b37018
mechanical-peregrin <serbinav86@gmail.com>
2022-10-02 21:28:14 +0400

    добавил необходимые иконки, добавил во фрагмент RecyclerView, отрисовал item для recycler_reddit

--- app/src/main/java/com/example/pagingroom/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/pagingroom/ui/home/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.pagingroom.databinding.FragmentHomeBinding
@@ -12,9 +11,6 @@ import com.example.pagingroom.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -28,9 +24,9 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
+        var textView: String
         homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+            textView = it
         }
         return root
     }
